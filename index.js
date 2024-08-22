ee89cdd7b39355f8a30adfe598f1575f314d6e0a
Nelson Trujillo <nelson.trujillo@cobistopaz.com>
2024-08-21 22:51:42 -0500

    feat: data validation

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const Joi = require('joi');
 const express = require('express');
 const http = require('http');
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/vidly')
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could not connect to MongoDB...'));
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000;
 
